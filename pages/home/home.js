2b54ad2c54cdc7063d504b2e10bda3b5f9eddc5e
Rossi Oddet <rossi.oddet@gmail.com>
2013-07-29 01:45:30 +0200

    update

--- pages/home/home.js
+++ pages/home/home.js
@@ -4,18 +4,13 @@ app.controller('HomeCtrl', ['$http', '$scope', 'backendService', function ($http
     $scope.carouselInterval = 2000;
 
     $scope.slides = [
-        {image: 'images/jcertif_express.png', title : 'JCertif Express 2013'},
-        {image: 'https://lh4.googleusercontent.com/-QB5QlRW0GIA/UHP0gloKm6I/AAAAAAAABHQ/5ujOaL-U4YA/w973-h651-no/DSC_0026.jpg', title : 'Africa Android Challenge'},
-        {image: 'https://lh4.googleusercontent.com/-TAFhAN3_qvU/UFqPrUtPg6I/AAAAAAAAAb8/92A7rrQB1Q0/s953/DSC_0268.jpg', title : 'JCertif Lab'},
-        {image: 'https://lh6.googleusercontent.com/-Zj1JcJtP4RY/UHP1R5feaRI/AAAAAAAABNg/q7ZURF0VI74/s953/DSC_0146.JPG', title : 'JCertif 2012'}
+        {image: 'https://lh3.googleusercontent.com/-GgL8kxpsgzU/UHP0fTGipVI/AAAAAAAABHA/U0fiBJO0NIw/w973-h651-no/DSC_0020.jpg'},
+        {image: 'https://lh4.googleusercontent.com/-QB5QlRW0GIA/UHP0gloKm6I/AAAAAAAABHQ/5ujOaL-U4YA/w973-h651-no/DSC_0026.jpg'},
+        {image: 'https://lh4.googleusercontent.com/-TAFhAN3_qvU/UFqPrUtPg6I/AAAAAAAAAb8/92A7rrQB1Q0/s953/DSC_0268.jpg'},
+        {image: 'https://lh6.googleusercontent.com/-Zj1JcJtP4RY/UHP1R5feaRI/AAAAAAAABNg/q7ZURF0VI74/s953/DSC_0146.JPG'}
     ];
 
     backendService.getSponsors(function(sponsorsList) {
        $scope.sponsors = sponsorsList;
    });
-
-
-    backendService.getSpeakers(function(speakersList) {
-        $scope.speakers = speakersList;
-    });
 }]);
