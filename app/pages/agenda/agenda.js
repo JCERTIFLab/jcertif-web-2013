edd5583a593cffcd2ac16f6671290bb86b62c926
Rossi Oddet <rossi.oddet@gmail.com>
2013-07-31 01:57:56 +0200

    Initialisation de la page Agenda. Reste à faire la mise en forme

---
+++ app/pages/agenda/agenda.js
@@ -0,0 +1,95 @@
+'use strict';
+
+app.controller('AgendaCtrl', ['$http', '$scope', 'backendService', function ($http, $scope, backendService) {
+
+    backendService.getSessions(function(sessionsList) {
+
+        var days = [];
+        var sessLength = sessionsList.length;
+        for(var i = 0; i < sessLength; i++) {
+            var startDate = getDate(sessionsList[i].start);
+            if(!contains(days, startDate)) {
+                days[days.length] = startDate;
+            }
+        }
+
+        var coolDays = [];
+
+        function addSession(rooms, session) {
+
+            if(isRoomsHasSessionRoom(rooms, session)) {
+                for(var i=0; i < rooms.length; i++) {
+                    if (rooms[i].name == session.room) {
+                        rooms[i].sessions[rooms[i].sessions.length] = session;
+                    }
+                }
+            } else {
+                rooms[rooms.length] = {
+                    name : session.room,
+                    sessions : [session]
+                }
+            }
+
+
+        }
+
+        function isRoomsHasSessionRoom(rooms, session) {
+            for(var i=0; i < rooms.length; i++) {
+                if (rooms[i].name == session.room) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        for(var j = 0; j < days.length; j++) {
+            var roomsDay=[];
+            for(var i = 0; i < sessLength; i++) {
+                var startDate = getDate(sessionsList[i].start);
+                if(days[j] == startDate) {
+                    addSession(roomsDay,sessionsList[i]);
+                }
+            }
+            coolDays[coolDays.length] = {
+                day : days[j],
+                rooms : roomsDay
+            };
+        }
+
+
+
+        $scope.days = coolDays;
+        $scope.sessions = sessionsList;
+    });
+
+    function contains(a, obj) {
+        for (var i = 0; i < a.length; i++) {
+            if (a[i] === obj) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function getDate(dateHour) {
+        return dateHour.substr(0, 5);
+    }
+
+    var activeRoom = "01";
+
+    $scope.activeRoom = function(room) {
+        activeRoom = room;
+        $scope.room = activeRoom;
+    }
+
+    $scope.getClass = function(room) {
+        if(activeRoom == room) {
+            return "active";
+        }else {
+            return "";
+        }
+
+    }
+
+}]);
