f3c4b70595ba5c0d312395fe4ad3bb796275880d
Rossi Oddet <rossi.oddet@gmail.com>
2013-08-02 00:17:37 +0200

    Page Accueil - redimensionnement des images caroussel, fond gris pour les sponsors

--- app/pages/home/home.js
+++ app/pages/home/home.js
@@ -1,19 +1,18 @@
 'use strict';
 
 app.controller('HomeCtrl', ['$http', '$scope', 'backendService', function ($http, $scope, backendService) {
-    $scope.carouselInterval = 2000;
+    $scope.carouselInterval = 7000;
 
     $scope.slides = [
-        {image: 'https://lh3.googleusercontent.com/-GgL8kxpsgzU/UHP0fTGipVI/AAAAAAAABHA/U0fiBJO0NIw/w973-h651-no/DSC_0020.jpg'},
-        {image: 'https://lh4.googleusercontent.com/-QB5QlRW0GIA/UHP0gloKm6I/AAAAAAAABHQ/5ujOaL-U4YA/w973-h651-no/DSC_0026.jpg'},
-        {image: 'https://lh4.googleusercontent.com/-TAFhAN3_qvU/UFqPrUtPg6I/AAAAAAAAAb8/92A7rrQB1Q0/s953/DSC_0268.jpg'},
-        {image: 'https://lh6.googleusercontent.com/-Zj1JcJtP4RY/UHP1R5feaRI/AAAAAAAABNg/q7ZURF0VI74/s953/DSC_0146.JPG'}
+        {image: 'images/banner.png'},
+        {image: 'images/slide1.jpg'},
+        {image: 'images/slide2.jpg'}
     ];
 
     backendService.getSponsors(function(sponsorsList) {
        $scope.sponsors = sponsorsList;
    });
 
-    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+   !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 
 }]);
