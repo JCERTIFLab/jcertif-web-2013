5ce66c9bc6227c96912f5638bdb3f7dc7f5494f3
Rossi Oddet <rossi.oddet@gmail.com>
2013-08-03 01:41:07 +0200

    [REGISTER] Affichage d'un message de confirmation ou d'erreur suivant le résultat de l'inscription

--- app/pages/register/register.js
+++ app/pages/register/register.js
@@ -1,13 +1,30 @@
 'use strict';
 
-app.controller('RegisterCtrl', ['$http', '$scope', function ($http, $scope) {
+app.controller('RegisterCtrl', ['$http', '$scope', '$dialog', 'i18nService', function ($http, $scope, $dialog, i18nService) {
     $http.defaults.useXDomain = true;
     var requiredFields = ['email', 'password', 'firstname', 'lastname', 'city', 'country'];
     $scope.submitForm = function () {
         $http.post(backendUrl + '/participant/register?jsonp=JSON_CALLBACK',  $scope.user)
             .success(function (result) {
-                alert(result);
+                openConfirmDialog();
+            })
+            .error(function (result) {
+                openErrorDialog();
             });
+    }
+
+    function openConfirmDialog() {
+        $dialog.messageBox(
+            i18nService.getText('form.register.ok'),
+            i18nService.getText('form.register.ok.msg')
+        ).open();
+    }
+
+    function openErrorDialog() {
+        $dialog.messageBox(
+            i18nService.getText('form.register.ko'),
+            i18nService.getText('form.register.ko.msg')
+        ).open();
     }
 
     $scope.isValid = function() {
@@ -18,9 +35,9 @@ app.controller('RegisterCtrl', ['$http', '$scope', function ($http, $scope) {
             if( $scope.user[requiredFields[i]] == undefined || $scope.user[requiredFields[i]].trim() == '') {
                 return false;
             }
-            if($scope.user['password'].length < 6) {
-                return false;
-            }
+        }
+        if($scope.user['password'].length < 6) {
+            return false;
         }
         return  true;
     }
---
+++ app/services/i18nService.js
@@ -0,0 +1,68 @@
+app.factory('i18nService', function () {
+
+    var FR = {
+        "header.conference": "CONFÉRENCE",
+        "header.city": "IFC (EX-CCF) - Brazzaville",
+        "header.date": "9 - 15 Sept. 2013",
+        "header.login": "Se connecter",
+        "header.nav.home": "Accueil",
+        "header.nav.register": "S'inscrire",
+        "header.nav.speakers": "Présentateurs",
+        "header.nav.sessions": "Sessions",
+        "header.nav.cfp": "Proposez un sujet",
+        "header.nav.agenda": "Agenda",
+        "header.nav.contact": "Contact",
+        "home.news": "UNE CONFERENCE SUR PLUSIEURS METIERS ET COMPETENCES",
+        "home.sponsors": "NOS SPONSORS",
+        "home.newsletter": "NEWSLETTER",
+        "home.followus": "SUIVEZ-NOUS",
+        "home.actus": "ACTUALITES",
+        "home.newsletter.register": "Inscrivez-vous",
+        "home.newsletter.input": "Adresse email",
+        "home.newsletter.btn": "S'inscrire",
+        "form.email": "Email",
+        "form.confirmEmail": "Confirmez Email",
+        "form.password": "Mot de passe",
+        "form.confirmPassword": "Confirmez Mot de passe",
+        "form.firstname": "Prénom",
+        "form.lastname": "Nom",
+        "form.city": "Ville",
+        "form.country": "Pays",
+        "form.biography": "Bio",
+        "form.submit": "S'inscrire",
+        "form.submitCfp": "Proposez !",
+        'form.title' : 'Titre',
+        'form.summary' : 'Résumé',
+        'form.description' : 'Description',
+        'form.keyword' : 'Mots Clés',
+        'form.category' : 'Catégorie',
+        'form.password.min' : '6 caractères minimum',
+        'form.register.ok' : 'Inscription validée',
+        'form.register.ko' : 'Ooops !',
+        'form.register.ok.msg' : 'Votre inscription a bien été prise en compte. Vous allez recevoir un email qui vous indiquera les prochaines étapes à suivre.',
+        'form.register.ko.msg' : "Une erreur technique est survenue, veuillez nous envoyer un email à l'adresse info@jcertif.com."
+    }
+
+    var EN = {
+        'header.conference': 'CONFERENCE',
+        'header.city': 'IFC (EX-CCF) - Brazzaville',
+        'header.date': '9 - 15 Sept. 2013',
+        'header.login': 'Login'
+    }
+
+    var currentLang = FR;
+
+    return {
+        getText : function(code) {
+            return currentLang[code];
+        },
+        setLang : function(lang) {
+            if(currentLang == FR) {
+                currentLang = EN;
+            } else {
+                currentLang = FR;
+            }
+
+        }
+    };
+});
