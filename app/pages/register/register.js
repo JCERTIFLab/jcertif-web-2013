8fcad775626b31cc5245bf24acdac3aef7502764
Rossi Oddet <rossi.oddet@gmail.com>
2013-08-03 00:34:21 +0200

    [REGISTER] Le bouton "s'inscrire" est désormais grisé tant que tous les champs ne sont pas saisis

--- app/pages/register/register.js
+++ app/pages/register/register.js
@@ -2,6 +2,7 @@
 
 app.controller('RegisterCtrl', ['$http', '$scope', function ($http, $scope) {
     $http.defaults.useXDomain = true;
+    var requiredFields = ['email', 'password', 'firstname', 'lastname', 'city', 'country'];
     $scope.submitForm = function () {
         $http.post(backendUrl + '/participant/register?jsonp=JSON_CALLBACK',  $scope.user)
             .success(function (result) {
@@ -9,5 +10,15 @@ app.controller('RegisterCtrl', ['$http', '$scope', function ($http, $scope) {
             });
     }
 
-
+    $scope.isValid = function() {
+        if($scope.user == undefined) {
+            return false;
+        }
+        for(var i=0; i < requiredFields.length; i++) {
+            if( $scope.user[requiredFields[i]] == undefined || $scope.user[requiredFields[i]].trim() == '') {
+                return false;
+            }
+        }
+        return  true;
+    }
 }]);
