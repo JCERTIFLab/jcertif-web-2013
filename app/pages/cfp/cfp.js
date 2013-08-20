452965c6cde7f5b4736085787beb1c64c099879d
Rossi Oddet <rossi.oddet@gmail.com>
2013-08-21 01:13:23 +0200

    [CFP] Correction de plusieurs bugs de soumission de sessions. Simplification du workflow. Désormais l'utilisateur n'a accès au formulaire que s'il est connecté. La seule info modifiable sur sa personne reste sa biographie.

--- app/pages/cfp/cfp.js
+++ app/pages/cfp/cfp.js
@@ -1,89 +1,70 @@
 'use strict';
 
-app.controller('CfpCtrl', ['$http', '$scope', '$dialog', 'backendService', function ($http, $scope, $dialog, backendService) {
+app.controller('CfpCtrl', ['$http', '$scope', '$rootScope', '$dialog', 'backendService', function ($http, $scope, $rootScope, $dialog, backendService) {
 
-    var userFields = ['email', 'password', 'firstname', 'lastname', 'city', 'country'];
+    var userFields = ['biography'];
     var cfpFields = ['category', 'title', 'summary', 'keyword', 'description'];
 
     $scope.submitForm = function () {
-        if($scope.speakerFoundEmail == '') {
-            saveOrUpdateSpeaker('register');
-        } else {
-            saveOrUpdateSpeaker('update');
-        }
-
+        saveOrUpdateSpeaker('register');
     }
+
 
     function saveOrUpdateSpeaker(action) {
-        $http.post(backendUrl + '/speaker/' + action + '?jsonp=JSON_CALLBACK',  $scope.user)
+        $scope.user.password=$rootScope.access_token;
+        $http.post(backendUrl + '/speaker/' + action + '?jsonp=JSON_CALLBACK&access_token=' + $rootScope.access_token + '&provider=' + $rootScope.provider + '&user=' + $rootScope.user.email, $scope.user)
             .success(function (result) {
                 $scope.cfp.speakers = [$scope.user.email];
-                $http.post(backendUrl + '/session/new?jsonp=JSON_CALLBACK',  $scope.cfp)
+                $http.post(backendUrl + '/session/new?jsonp=JSON_CALLBACK', $scope.cfp)
                     .success(function (result) {
                         openConfirmBox();
-                    }).error(function(data, status, headers, config) {
-                        openErrorBox(data);
+                    }).error(function (data, status, headers, config) {
+                        openErrorBox();
                     });
-            }).error(function(data, status, headers, config) {
-                openErrorBox(data);
+            }).error(function (data, status, headers, config) {
+                if (action === 'register') {
+                    saveOrUpdateSpeaker('update');
+                } else {
+                    openErrorBox();
+                }
             });
     }
 
-    $scope.speakerFoundEmail = '';
-
-    $scope.checkSpeaker = function() {
-
-        if($scope.user != undefined && $scope.user.email != undefined &&  $scope.speakerFoundEmail != $scope.user.email) {
-            backendService.getSpeakers(function(speakers) {
-                for(var i= 0; i < speakers.length; i++) {
-                    if($scope.user != undefined && $scope.user.email != undefined && speakers[i].email == $scope.user.email) {
-                        $scope.user.firstname = speakers[i].firstname;
-                        $scope.user.lastname = speakers[i].lastname;
-                        $scope.user.city = speakers[i].city;
-                        $scope.user.country = speakers[i].country;
-                        $scope.user.biography = speakers[i].biography;
-                        $scope.speakerFoundEmail = $scope.user.email;
-                        return true;
-                    }
-                }
-                $scope.speakerFoundEmail = '';
-            });
-        }
-
-
-        return true;
-    };
-
-    function openErrorBox(){
+    function openErrorBox() {
         $dialog.messageBox('Erreur', 'Veuillez verifier les données saisies').open();
 
-    };
+    }
 
-    function openConfirmBox(){
+    function openConfirmBox() {
         $dialog.messageBox('Yes !', 'Nous avons bien reçu votre demande').open();
+    }
 
-    };
-
-    backendService.getCategories(function(categories) {
+    backendService.getCategories(function (categories) {
         $scope.categories = categories;
     });
 
-    $scope.isValid = function() {
-        if($scope.user == undefined) {
-            return false;
+    $scope.isValid = function () {
+        var valid = true;
+
+        if ($scope.user == undefined || $scope.cfp == undefined) {
+            valid = false;
         }
-        for(var i=0; i < userFields.length; i++) {
-            if( $scope.user[userFields[i]] == undefined || $scope.user[userFields[i]].trim() == '') {
-                return false;
-            }
+
+        if (valid) {
+            angular.forEach(userFields, function (userField) {
+                if ($scope.user[userField] == undefined || $scope.user[userField].trim() == '') {
+                    valid = false;
+                }
+            });
         }
-        for(var i=0; i < cfpFields.length; i++) {
-            if( $scope.cfp[cfpFields[i]] == undefined || $scope.cfp[cfpFields[i]].trim() == '') {
-                return false;
-            }
+
+        if (valid) {
+            angular.forEach(cfpFields, function (cfpField) {
+                if ($scope.cfp[cfpField] == undefined || $scope.cfp[cfpField].trim() == '') {
+                    valid = false;
+                }
+            });
         }
-        return  true;
+        return valid;
     };
-
-
 }]);
--- app/services/loginService.js
+++ app/services/loginService.js
@@ -8,7 +8,7 @@ app.factory('loginService', ['$http','$rootScope', '$cookieStore', function ($ht
         findParticipant(cookieEmail,cookieAccessToken, cookieProvider,
             function(participant, error) {
                 if(error == undefined) {
-                    saveLogin(participant, cookieAccessToken);
+                    saveLogin(participant, cookieAccessToken, cookieProvider);
                 } else {
                     logout();
                 }
@@ -20,24 +20,26 @@ app.factory('loginService', ['$http','$rootScope', '$cookieStore', function ($ht
         $rootScope.logged = false;
         $rootScope.user = undefined;
         $rootScope.access_token = undefined;
+        $rootScope.provider = undefined;
         $cookieStore.remove('access_token');
         $cookieStore.remove('user_email');
         $cookieStore.remove('provider');
     }
 
-    function saveLogin(participant, access_token){
+    function saveLogin(participant, access_token, provider){
         $rootScope.logged = true;
         $rootScope.user = participant;
         $rootScope.access_token = access_token;
+        $rootScope.provider = provider;
         $cookieStore.put('access_token',access_token);
         $cookieStore.put('user_email',participant.email);
-        $cookieStore.put('provider','userpass');
+        $cookieStore.put('provider', provider);
     }
 
     function findParticipant(email, access_token, provider, callback) {
         $http.jsonp(backendUrl + '/participant/get/' + email + '?access_token=' + access_token + '&provider=' + provider + '&jsonp=JSON_CALLBACK')
             .success(function (participant) {
-                saveLogin(participant, access_token);
+                saveLogin(participant, access_token, provider);
                 callback(participant, undefined);
             }
         ).error(function (result) {
