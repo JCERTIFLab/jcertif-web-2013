86dcd49dc9c50a0711515202677820bb8de6fec7
Rossi Oddet via Jenkins <rossi.oddet@gmail.com>
2013-08-21 10:59:46 -0400

    Update website

--- pages/home/home.js
+++ pages/home/home.js
@@ -9,7 +9,21 @@ app.controller('HomeCtrl', ['$http', '$scope', 'backendService', function ($http
         {image: 'images/slide2.jpg'}
     ];
 
-    backendService.getSponsors(function(sponsorsList) {
-       $scope.sponsors = sponsorsList;
-   });
+    backendService.getSponsors(function (sponsorsList) {
+        var sponsorLevel1 = [];
+        var sponsorLevel2 = [];
+        var sponsorLevel3 = [];
+        angular.forEach(sponsorsList, function (sponsor) {
+            if (sponsor.level == 'Titanium' || sponsor.level == 'Platine') {
+                sponsorLevel1[sponsorLevel1.length] = sponsor;
+            } else if (sponsor.level == 'Or' || sponsor.level == 'Argent') {
+                sponsorLevel2[sponsorLevel2.length] = sponsor;
+            } else {
+                sponsorLevel3[sponsorLevel3.length] = sponsor;
+            }
+        });
+        $scope.sponsorsLevel1 = sponsorLevel1;
+        $scope.sponsorsLevel2 = sponsorLevel2;
+        $scope.sponsorsLevel3 = sponsorLevel3;
+    });
 }]);
